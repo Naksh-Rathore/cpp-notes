df4d2e9c3e2dc827c362982e6fc1a14830c4ec4b
Naksh-Rathore <naksh.rathore786@gmail.com>
2025-10-15 21:40:50 -0500

    Add a project: perfect number calculator

---
+++ projects/perfect_number_calculator.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <limits>
+
+bool isPerfectNumber(int num);
+
+int main() {
+
+    int num{};
+
+    while (true) {
+        std::cout << "Enter a number: ";
+        std::cin >> num;
+        
+        
+        if (std::cin.fail()) { 
+            std::cin.clear(); 
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cout << "Invalid input! Please enter a number.\n";  
+            continue;
+        }
+
+        break;
+    }
+
+    std::string msg { isPerfectNumber(num) ? "DEFINITELY" : "NOT" };
+
+    std::cout << num << " is " << msg << " a perfect number" << "\n";
+
+    return 0;
+}
+
+bool isPerfectNumber(int num) {
+    std::vector<int> divisors {};
+    int sum{};
+
+    for (int i = 1; i < num; i++) {
+        if (num % i == 0)
+            divisors.push_back(i);
+    }
+
+    for (int divisor : divisors)
+        sum += divisor;
+        
+    return (sum == num);
+}
+
