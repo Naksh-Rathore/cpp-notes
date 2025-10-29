92611d95561ccda0911daa7157095170bc6fa2bf
Naksh-Rathore <naksh.rathore786@gmail.com>
2025-10-28 21:15:32 -0500

    Add project circle generator

---
+++ projects/circle_generator.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <array>
+#include <string_view>
+
+#define _USE_MATH_DEFINES
+#include <cmath>
+
+std::vector<std::array<double, 2>> createCircle(double circleCenterX, double circleCenterY, double radius);
+double getUserInput(std::string_view prompt);
+
+int main() {
+
+    double centerX { getUserInput("Enter X cord of the center of the circle: ") };
+    double centerY { getUserInput("Enter Y cord of the center of the circle: ") };
+    double radius { getUserInput("Enter radius of the circle: ") };
+
+    std::vector<std::array<double, 2>> points { createCircle(centerX, centerY, radius) };
+
+    for (int i = 0; i < points.size(); i++) {
+        std::cout << i << ") X: " << points.at(i).at(0) << "\n";
+        std::cout << i << ") Y: " << points.at(i).at(1) << "\n\n";
+    }
+
+    return 0;
+}
+
+std::vector<std::array<double, 2>> createCircle(double circleCenterX, double circleCenterY, double radius) {
+    std::vector<std::array<double, 2>> points{};
+    std::array<double, 2> cords{};
+
+    for (double theta = 0; theta <= 2 * M_PI; theta++) {
+        cords.at(0) = circleCenterX + radius * sin(theta);
+        cords.at(1) = circleCenterY + radius * cos(theta);
+
+        points.push_back(cords);
+    }
+
+    return points;
+}
+
+double getUserInput(std::string_view prompt) {
+    double input{};
+
+    while (true) {
+        std::cout << prompt;
+        std::cin >> input;
+
+        if (!std::cin.fail()) break;
+
+        std::cin.clear();  
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); 
+        std::cout << "Invalid input! Please enter a number.\n";  
+    }
+
+    return input;
+}
